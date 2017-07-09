1f290932b85467514ab64146bc5abd799e1c3850
Jose Albizures <albizures3601@gmail.com>
2017-07-09 15:28:50 -0600

    init

---
+++ index.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: 'stylelint-config-sass-guidelines',
+  rules: {
+    'scss/at-mixin-argumentless-call-parentheses': 'always',
+    'scss/dollar-variable-colon-space-after': 'always',
+    'color-no-hex': true,
+    'color-named': 'never',
+  }
+}
